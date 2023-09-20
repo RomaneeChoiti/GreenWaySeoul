59587f426c1171119f702716b1607e84e4dbbb27
RomaneeChoiti <tmd1568@gmail.com>
2023-09-20 17:37:21 +0900

    Feat: ESlint 적용 및 1개 에러 발생

--- App.tsx
+++ App.tsx
@@ -6,30 +6,32 @@ import React from "react";
 // import * as Location from "expo-location";
 
 export default function App() {
-  // const userLocation = async () => {
-  //   const { granted } = await Location.requestForegroundPermissionsAsync();
-  //   if (!granted) {
-  //     setOk(false);
-  //   }
-  //   // 유저 위치 get value
-  //   const {
-  //     coords: { latitude, longitude },
-  //   } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+  /*
+  const userLocation = async () => {
+  const { granted } = await Location.requestForegroundPermissionsAsync();
+    if (!granted) {
+      setOk(false);
+    }
+    // 유저 위치 get value
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
 
-  //   const location = await Location.reverseGeocodeAsync(
-  //     {
-  //       latitude,
-  //       longitude,
-  //     },
-  //     { useGoogleMaps: false }
-  //   );
-  //   setCity(location[0].city);
-  //   setCity2(location[0].district);
-  // };
+    const location = await Location.reverseGeocodeAsync(
+      {
+        latitude,
+        longitude,
+      },
+      { useGoogleMaps: false }
+    );
+    setCity(location[0].city);
+    setCity2(location[0].district);
+  };
 
-  // useEffect(() => {
-  //   userLocation();
-  // }, []);
+  useEffect(() => {
+    userLocation();
+  }, []);
+  */
 
   return (
     <View style={styles.continer}>
