cfc9e0c7de23ff8d2201f26ff19f9c4440e18449
RomaneeChoiti <tmd1568@gmail.com>
2023-10-12 15:02:19 +0900

    Feat: 지오로케이션 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,40 @@
 import React from 'react'
+import MapView from 'react-native-maps'
 import { StatusBar } from 'expo-status-bar'
-// import { useEffect } from "react";
 import { StyleSheet, View } from 'react-native'
-// import * as Location from "expo-location";
 
 export default function App() {
   return (
     <View style={styles.continer}>
-      <StatusBar style="auto" />
+      <MapView style={styles.map}>
+        <StatusBar style="auto" />
+        <View style={styles.title}>
+          <View style={styles.button}></View>
+          <View style={styles.button}></View>
+        </View>
+      </MapView>
     </View>
   )
 }
 const styles = StyleSheet.create({
   continer: {
     flex: 1,
+    backgroundColor: 'green',
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+  title: {
+    flex: 0.1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 50,
+  },
+  button: {
+    flex: 0.3,
     backgroundColor: 'white',
+    height: '100%',
+    marginHorizontal: 20,
   },
 })
