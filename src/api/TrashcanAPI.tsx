274ff96c5532b3f3c7609ea8ded6eb165063d2a0
RomaneeChoiti <tmd1568@gmail.com>
2024-07-05 15:49:17 +0900

    "Feat: Lambda filteringFunc 생성"

--- src/api/TrashcanAPI.tsx
+++ src/api/TrashcanAPI.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { TrashCanData, UserLocation } from '../components/Type'
+import { UserLocation } from '../components/Type'
 
 const trashCanAPI: string | undefined = process.env.TRASHCAN_AWS_API_URL
 
@@ -7,17 +7,17 @@ const fetchFilteredTrashCans = async ({
   location,
 }: {
   location: UserLocation
-}): Promise<TrashCanData[]> => {
+}) => {
   try {
-    const response = await axios.get(
-      `${trashCanAPI}`,
-      { params: { location } }, // 위치 데이터를 쿼리 매개변수로 전달
-    )
-
-    // 서버에서 필터링 및 정렬된 데이터를 직접 받아옴
+    const response = await axios.get(`${trashCanAPI}`, {
+      params: {
+        latitude: location.latitude,
+        longitude: location.longitude,
+      },
+    })
     return response.data
   } catch (error) {
-    console.error('Error fetching or filtering trash can data:', error)
+    console.error('쓰레기통 데이터를 가져오는 동안 에러 발생:', error)
     return [] // 에러 발생 시 빈 배열 반환
   }
 }
