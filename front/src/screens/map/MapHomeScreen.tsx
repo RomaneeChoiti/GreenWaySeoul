4472559acca435ec78a3c3edd175b39d668e8aa1
RomaneeChoiti <tmd1568@gmail.com>
2025-04-09 14:56:13 +0900

    feat/RN vector icon 셋팅

--- front/src/screens/map/MapHomeScreen.tsx
+++ front/src/screens/map/MapHomeScreen.tsx
@@ -4,6 +4,7 @@ import MapView, { PROVIDER_GOOGLE} from 'react-native-maps';
 import { colors } from '@/constants';
 import useUserLocation from '@/hooks/useUserLocation';
 import usePermission from '@/hooks/usePermission';
+import MateriallIcons from 'react-native-vector-icons/MaterialIcons';
 
 function MapHomeScreen() {
   const mapRef = useRef<MapView | null>(null);
@@ -42,12 +43,21 @@ function MapHomeScreen() {
         ]}
       >
         <View style={styles.buttonBackground}>
-          <Text style={styles.buttonText}>산책하기</Text>
+        <MateriallIcons
+          name="directions-run"
+          color={colors.WHITE}
+          size={50}
+        />
+          <Text style={styles.buttonText}>플로링 시작</Text>
         </View>
       </Pressable>
       <View>
         <Pressable style={styles.locationButton} onPress={handlePressUserLocation}>
-            <Text style={styles.buttonText}>내위치</Text>
+            <MateriallIcons
+              name="my-location"
+              color={colors.WHITE}
+              size={30}
+            />
         </Pressable>
       </View>
     </>
@@ -63,24 +73,27 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     bottom: 90,
     borderRadius: 30,
-    shadowColor: colors.BLACK,
-    shadowOffset: { width: 0, height: 4 },
-    shadowOpacity: 0.3,
-    shadowRadius: 5,
-    elevation: 5, // Android 그림자
+    shadowColor: colors.SECONDARY,
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.5,
+    shadowRadius: 20,
+    elevation: 10, // Android 그림자
   },
   actionButtonPressed: {
     transform: [{ scale: 0.95 }], // 버튼을 눌렀을 때 약간 작아짐
   },
   buttonBackground: {
     backgroundColor: colors.PRIMARY, // 버튼 배경색
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 10,
     paddingVertical: 20,
     paddingHorizontal: 40,
     borderRadius: 30,
   },
   buttonText: {
     color: colors.WHITE, // 텍스트 색상
-    fontSize: 18,
+    fontSize: 20,
     fontWeight: 'bold',
     textAlign: 'center',
   },
@@ -89,9 +102,9 @@ const styles = StyleSheet.create({
     alignSelf: 'flex-end',
     bottom: 40,
     right: 30,
-    backgroundColor: colors.PRIMARY,
-    paddingVertical: 20,
-    paddingHorizontal: 10,
+    backgroundColor: colors.PRIMARY_DARK,
+    paddingVertical: 15,
+    paddingHorizontal: 15,
     borderRadius: 100,
     shadowColor: colors.BLACK,
     shadowOffset: { width: 0, height: 4 },
