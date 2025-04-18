c306755ac343814b93e45cda96ecb90ac1d4b756
RomaneeChoiti <tmd1568@gmail.com>
2025-04-18 14:44:18 +0900

    feat/점수구현

---
+++ front/src/components/StarRating.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+interface StarRatingProps {
+  score: number;
+  onRate: (rating: number) => void;
+}
+
+const StarRating = ({ score, onRate }: StarRatingProps) => {
+  return (
+    <View>
+        <Text style={styles.text}>오늘의 플로깅 점수를 매겨보세요!</Text>
+        <View style={styles.ratingContainer}>
+        {[1, 2, 3, 4, 5].map((rating) => (
+            <Ionicons
+            key={rating}
+            name={rating <= score ? 'trash-sharp' : 'trash-outline'}
+            size={50}
+            color={rating <= score ? 'green' : 'black'}
+            onPress={() => onRate(rating)}
+            />
+        ))}
+        </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+    text: {
+    fontSize: 20,
+    textAlign: 'center',
+    paddingVertical: 10,
+    },
+    ratingContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 10,
+    marginVertical: 10,
+    },
+});
+
+export default StarRating;
--- front/src/screens/map/AddPostScreen.tsx
+++ front/src/screens/map/AddPostScreen.tsx
@@ -12,46 +12,51 @@ import ModalComponent from '@/components/ModalComponent';
 import useMutateCreatePost from '@/hooks/queries/useMutateCreatePost';
 import { useTrashcanStore } from '@/store/useTrashcanStore';
 import { usePloggingStateStore } from '@/store/usePloggingStore';
+import StarRating from '@/components/StarRating';
 
-interface AddPostScreenProps {}
-
-function AddPostScreen({}: AddPostScreenProps) {
+function AddPostScreen() {
   const navigation = useNavigation();
   const [isModalVisible, setModalVisible] = useState(false);
   const currentDate = new Date().toISOString();
   const formattedDate = new Intl.DateTimeFormat('en-CA').format(new Date(currentDate));
+  const day = new Intl.DateTimeFormat('ko-KR', { weekday: 'long' }).format(new Date(currentDate));
   const descriptionRef = useRef<TextInput | null>(null);
   const createPost = useMutateCreatePost();
+  /*
+   TODO: 이동 거리는 후순위로
+   const [distance, setDistance] = useState('0km');
+  */
+  const [score, setScore] = useState(0);
+
+  // zustand Store
   const { address: trashcanAddress, location: place } = useTrashcanStore();
   const { ploggingTime } = usePloggingStateStore();
   const formattedTime = ploggingTime
     ? new Date(ploggingTime).toISOString().substr(11, 8) // Format as HH:mm:ss
     : '00:00:00';
-  const [distance, setDistance] = useState('0km');
+
   const addPost = useForm({
       initialValues: { title: '', description: '' },
       validate: validateAddPost,
     });
 
+  // handler
   const handleCancel = () => {
     setModalVisible(true); // Show the modal
   };
-
   const handleConfirm = () => {
     setModalVisible(false);
     navigation.goBack(); // Navigate back to the map screen
   };
-
   const handleCloseModal = () => {
     setModalVisible(false); // Close the modal
   };
-
   const handleSubmit = () => {
     const body = {
       date: formattedDate,
       address: trashcanAddress || '주소 없음',
       place: place || '위치 없음',
-      distance: distance,
+      // distance: distance,
       time: formattedTime,
       title: addPost.values.title,
       description: addPost.values.description,
@@ -65,6 +70,10 @@ function AddPostScreen({}: AddPostScreenProps) {
     });
   };
 
+  const handleStarPress = (rating: number) => {
+    setScore(rating); // Update the score when a star is clicked
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
@@ -76,10 +85,12 @@ function AddPostScreen({}: AddPostScreenProps) {
               <Text style={styles.addressText}>{place || '위치 없음'}</Text>
             </View>
           </View>
-          <Text>날짜: {formattedDate}</Text>
-          {/* TODO: 맵화면에 폴로깅 진행시간 check */}
-          <Text>소요 시간 : {formattedTime}</Text>
-          <Text>이동 거리 : {distance}</Text>
+          <View style={styles.infoContainer}>
+            <Text style={styles.infoText}>{formattedDate} ( {day} )</Text>
+            {/* TODO: 맵화면에 폴로깅 진행시간 check */}
+            <Text style={styles.infoText}>{formattedTime}</Text>
+          </View>
+            {/* <Text>이동 거리 : {distance}</Text> */}
           <InputField
             placeholder="제목을 입력하세요."
             error={addPost.errors.title}
@@ -98,6 +109,7 @@ function AddPostScreen({}: AddPostScreenProps) {
               returnKeyType ="next"
               {...addPost.getTextInputProps('description')}
           />
+          <StarRating score={score} onRate={handleStarPress} />
           <View style={styles.buttonContainer}>
             <CustomButton label="취소" variant="outlined" size="medium" onPress={handleCancel} />
             <CustomButton label="등록" variant="filled" size="medium" onPress={handleSubmit} />
@@ -143,6 +155,16 @@ const styles = StyleSheet.create({
   buttonContainer: {
     flexDirection: 'row',
     gap: 10,
+  },
+  infoContainer: {
+    gap: 15,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+  },
+  infoText:{
+    fontSize: 25,
+    color: colors.PRIMARY,
+    fontWeight: 'bold',
   },
 });
 
