891a4852ee12fd43dff3278183e0d937488a4181
RomaneeChoiti <tmd1568@gmail.com>
2025-05-12 13:43:38 +0900

    fix/27min->30min

--- front/src/constants/numbers.ts
+++ front/src/constants/numbers.ts
@@ -1,5 +1,5 @@
 const numbers = {
-    ACCESS_TOKEN_REFRESH_TIME: 1000 * 60 * 30 - 1000 * 60 * 3,
+    ACCESS_TOKEN_REFRESH_TIME: 1000 * 60 * 30,
     INITIAL_DELTA: {
       latitudeDelta: 0.0922,
       longitudeDelta: 0.0421,
