845056e4b9b309e9be4d2b6172fc473bc65d16ce
RomaneeChoiti <tmd1568@gmail.com>
2025-05-12 13:39:41 +0900

    feat/로그아웃 기능 추가

--- front/src/navigations/drawer/CustomDrawerContent.tsx
+++ front/src/navigations/drawer/CustomDrawerContent.tsx
@@ -3,25 +3,48 @@ import { View, Text, Image, StyleSheet, Pressable } from 'react-native';
 import { DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';
 import { DrawerContentComponentProps } from '@react-navigation/drawer';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import useAuth from '@/hooks/queries/useAuth';
 
 
 const CustomDrawerContent = (props: DrawerContentComponentProps) => {
     const kakaoImageUrl = null; // kakao 로그인 미구현 상태
 
+    const {logoutMutation, getProfileQuery} = useAuth();
+    const {email, nickname, imageUri, kakaoImageUri} = getProfileQuery.data || {};
+
+    const handleLogout = () => {
+      logoutMutation.mutate(null);
+    };
+
   return (
     <DrawerContentScrollView {...props} contentContainerStyle={styles.container}>
       <View style={styles.userInfoSection}>
-        {kakaoImageUrl === null && (
+      { kakaoImageUrl === null && (
         <Image
           source={require('@/assets/dfUser.png')} // Replace with actual user image URL
           style={styles.userImage}
         />
         )}
-        <Text style={styles.userEmail}>user@example.com</Text> {/* Replace with actual user email */}
+      {imageUri && (
+        <Image
+          source={{ uri: imageUri }}
+          style={styles.userImage}
+        />
+      )}
+      { kakaoImageUri && (
+        <Image
+          source={{ uri: kakaoImageUri }}
+          style={styles.userImage}
+        />
+      )}
+        <Text style={styles.userEmail}>{nickname ?? email}</Text>
       </View>
       <DrawerItemList {...props} />
       <View style={styles.logoutButtonContainer}>
-        <Pressable style={styles.logoutButton}>
+        <Pressable
+          onPress={handleLogout}
+          style={styles.logoutButton}
+          >
           <MaterialIcons name={'logout'}/>
           <Text>로그아웃</Text>
         </Pressable>
