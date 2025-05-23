ee2c57464474150310a863cd46d68dea5dcae48f
Seungwon Choi <112051914+RomaneeChoiti@users.noreply.github.com>
2025-05-23 10:37:30 +0900

    Feat/#32 server auth signup signin (#78)
    * feat/uer 엔티티
    
    * feat/DTO
    
    * feat/회원가입
    
    * feat/로그인 (토큰 미구현 버전)
    
    * refactor/오타 수정
    
    * refactor/Error를 ConflictException으로 변경

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,4 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { AuthDto } from './dto/auth.dto';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>
+  ) {}
+  async signup(authDto: AuthDto) {
+    const { email, password } = authDto;
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.userRepository.create({
+      email,
+      password: hashedPassword,
+    });
+    try {
+      await this.userRepository.save(user);
+    } catch (error) {
+      console.error('Error saving user:', error);
+      if (error.code === '23505') {
+        throw new ConflictException('이미 존재하는 이메일입니다.');
+      }
+      throw new InternalServerErrorException('회원가입 중 오류가 발생했습니다.');
+    }
+  }
+
+  async signin(authDto: AuthDto) {
+    const { email, password } = authDto;
+    const user = await this.userRepository.findOneBy({ email });
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      throw new UnauthorizedException('이메일 또는 비밀번호가 잘못되었습니다.');
+    }
+  }
+}
