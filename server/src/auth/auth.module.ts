0dd0239f1ab852121652d849ea47d4e3a51f7aac
RomaneeChoiti <tmd1568@gmail.com>
2025-05-23 12:16:37 +0900

    refactor/JwtModule을 ConfigService와 registerAsync로 구성하여 JWT 설정 일관성 개선

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -6,11 +6,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt.strategy';
 import { PassportModule } from '@nestjs/passport';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
-    JwtModule.register({}),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: configService.get<string>('JWT_EXPIRES_IN') },
+      }),
+    }),
     TypeOrmModule.forFeature([User]),
   ],
   controllers: [AuthController],
