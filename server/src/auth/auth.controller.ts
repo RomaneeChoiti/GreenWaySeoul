37225cc70120ef7d974f619275cbf2a53e396992
Seungwon Choi <112051914+RomaneeChoiti@users.noreply.github.com>
2025-05-26 12:06:28 +0900

    feat/프로필, 로그아웃, 회월탈퇴 (#81)
    * feat/프로필, 로그아웃, 회월탈퇴
    
    * refactor/ 쿼리 매개변수 이름과 플레이스홀더 불일치 수정 (:userId → :id)

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,9 +1,19 @@
-import { Body, Controller, Post, UseGuards, ValidationPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Patch,
+  Post,
+  UseGuards,
+  ValidationPipe,
+} from '@nestjs/common';
 import { AuthDto } from './dto/auth.dto';
 import { AuthService } from './auth.service';
 import { GetUser } from 'src/@common/decorator/get-user.decorator';
 import { User } from './user.entity';
 import { AuthGuard } from '@nestjs/passport';
+import { EditProfileDto } from './dto/editProfile.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -23,5 +33,29 @@ export class AuthController {
   @UseGuards(AuthGuard())
   refresh(@GetUser() user: User) {
     return this.authService.refreshToken(user);
+  }
+
+  @Get('/me')
+  @UseGuards(AuthGuard())
+  getProfile(@GetUser() user: User) {
+    return this.authService.getProfile(user);
+  }
+
+  @Patch('/me')
+  @UseGuards(AuthGuard())
+  editProfile(@Body() editProfileDto: EditProfileDto, @GetUser() user: User) {
+    return this.authService.editProfile(editProfileDto, user);
+  }
+
+  @Post('/logout')
+  @UseGuards(AuthGuard())
+  logout(@GetUser() user: User) {
+    return this.authService.deleteRefreshToken(user);
+  }
+
+  @Delete('/me')
+  @UseGuards(AuthGuard())
+  deleteAccount(@GetUser() user: User) {
+    return this.authService.deleteAccount(user);
   }
 }
