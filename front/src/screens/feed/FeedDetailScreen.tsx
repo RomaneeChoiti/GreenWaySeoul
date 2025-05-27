72f93ade6a53e1c8f4faac56bb998437dbd98c8b
RomaneeChoiti <tmd1568@gmail.com>
2025-05-27 15:00:33 +0900

    refactor/안쓰는함수정리

--- front/src/screens/feed/FeedDetailScreen.tsx
+++ front/src/screens/feed/FeedDetailScreen.tsx
@@ -4,7 +4,7 @@ import useGetPost from '@/hooks/queries/useGetPost';
 import { formatDate } from '@/utils/date'; // Import the utility function
 import { FeedStackParamList } from '@/navigations/stack/FeedStackNavigator';
 import { StackScreenProps } from '@react-navigation/stack';
-import { Alert, Image, Platform, Pressable, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { Alert, Image, Pressable, ScrollView, StyleSheet, Text, View } from 'react-native';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import Octicons from 'react-native-vector-icons/Octicons';
