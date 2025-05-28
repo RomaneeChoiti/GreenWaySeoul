0eed064fece0a03c3bdf225eee98c8b5b5d17977
RomaneeChoiti <tmd1568@gmail.com>
2025-05-28 11:02:34 +0900

    feat/EC2설정

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,6 +5,23 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3030;
 
+  // if (process.env.NODE_ENV === 'production') {
+  //   app.enableCors({
+  //     origin: ['https://your-production-domain.com'],
+  //     credentials: true,
+  //   });
+  // } else {
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
+  // }
+
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
+
   await app.listen(port);
   console.log(`http://localhost:${port}`);
 }
