8d743a71a1cdcf4c7498650a470269a9e6a8ce45
RomaneeChoiti <tmd1568@gmail.com>
2025-05-28 11:24:32 +0900

    refactor/TODO설정

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3030;
-
+  // TODO : CORS 설정을 환경변수로 관리할 수 있도록 변경
   app.enableCors({
     origin: true,
     credentials: true,
