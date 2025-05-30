038c44927d7f8432f77b093c307437b1d9875417
RomaneeChoiti <tmd1568@gmail.com>
2025-05-30 11:11:41 +0900

    fix/path 모듈 추가

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,9 +5,10 @@ import { AuthModule } from './auth/auth.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ImageModule } from './image/image.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
-import path, { join } from 'path';
+import { join } from 'path';
 import { FavoriteModule } from './favorite/favorite.module';
 import * as fs from 'fs';
+import * as path from 'path';
 
 const rdsCaCertPath = path.resolve(
   __dirname,
